8f01fd422307b4b7d5655cd549067278753b7f99
Evgeniy <epankov174@gmail.com>
2021-12-26 21:24:23 +0500

    Add files site

---
+++ sendmail.php
@@ -0,0 +1,44 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPMailer.php';
+
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
+    $mail->setLanguage('ru', 'phpmailer/language/');
+    $mail->isHTML(true);
+
+    // Кому отправить
+    $mail->addAddress('info@pea-sites.ru');
+    $mail->Subject = 'Сообщение формы с сайта';
+
+    // Тело письма
+    $body = '<h1>Обратная связь</h1>';
+
+    if(trim(!empty($_POST['name']))){
+        $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+    }
+    if(trim(!empty($_POST['email']))){
+        $body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+    }
+    if(trim(!empty($_POST['message']))){
+        $body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+    }
+    
+    $mail->Body = $body;
+
+    // Отправляем
+    if (!$mail->send()){
+        $message = 'Ошибка';
+    }else{
+        $message = 'Данные отправлены!';
+    }
+
+    $response = ['message' => $message];
+
+    header('Content-type: application/json');
+    echo json_encode($response);
+
+?>
